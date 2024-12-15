4998398727dabadcf928391368eea9e709103709
Harix034 <harix877@outlook.com>
2024-12-15 15:53:18 -0500

    Build and run Docker containers successfully

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        // The entry point for the application
+        static void Main(string[] args)
+        {
+            // Print a message to the console
+            Console.WriteLine("Hello, World!");
+
+            // You can add more logic here for your application
+        }
+    }
+}
